1e51796dd25c4b5f8d6c5546aa94fe2350cb02ab
Hmuriy Monakh <max_08931993@mail.ru>
2022-08-18 16:06:31 +0300

    vce

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Введите первое число");
+      int x = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Введите второе число");
+      int y = Convert.ToInt32(Console.ReadLine());
+      if (x == y*y)
+      {Console.Write(y);
+        Console.Write(" является квадратом числа ");
+        Console.Write(x);}
+      else
+      {Console.Write(y);
+        Console.Write(" не является квадратом числа ");
+        Console.Write(x);}
+    }
+}
